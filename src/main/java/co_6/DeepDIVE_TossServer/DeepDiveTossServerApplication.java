ab08d24e63887e1d04729aa2801d2f53aae7c442
jje0ngyu <naniaerang@naver.com>
2023-07-01 13:02:13 +0900

    restart

---
+++ src/main/java/co_6/DeepDIVE_TossServer/DeepDiveTossServerApplication.java
@@ -0,0 +1,13 @@
+package com6.DeepDIVE_TossServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepDiveTossServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeepDiveTossServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co_6/DeepDIVE_TossServer/DeepDiveTossServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com6.DeepDIVE_TossServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepDiveTossServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
